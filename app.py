3fe60fb5865eb013d2212390b2e8cb4aa72c69dc
baptistejouin <baptiste.jouin44@gmail.com>
2023-06-21 14:28:49 +0200

    feat: fetch, decode

--- app.py
+++ app.py
@@ -16,7 +16,86 @@ from notion_client import Client
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
-"""
-	Load Notion API
-"""
-notion = Client(auth=config["NOTION_TOKEN"])
+
+async def getDatabaseMetadata(notion: Client):
+    """
+    Get the database metadata
+
+    @type notion: Client
+    @param notion: Notion client
+
+    @rtype: dict
+    @return: Database metadata
+    """
+    return await notion.databases.retrieve(
+        **{
+            "database_id": config["DATABASE_ID"],
+        }
+    )
+
+
+def getDatabaseEntries(notion: Client):
+    """
+    Get all the database entries
+
+    @type notion: Client
+    @param notion: Notion client
+
+    @rtype: list
+    @return: Database entries
+    """
+    databaseEntries = []
+    query = {"has_more": True, "next_cursor": None}
+    while query["has_more"]:
+        query = notion.databases.query(
+            **{
+                "database_id": config["DATABASE_ID"],
+                "page_size": 100,
+                "start_cursor": query["next_cursor"],
+                "filter": config["FILTER"],
+            }
+        )
+        databaseEntries.append(query["results"])
+        # flatten the list
+        databaseEntries = [item for sublist in databaseEntries for item in sublist]
+    return databaseEntries
+
+
+def decode(databaseEntries):
+    """
+    Decode the database entries to get only the title and the date
+
+    @type databaseEntries: list
+    @param databaseEntries: Database entries
+
+    @rtype: list
+    @return: Decoded database entries
+    """
+    filtered = []
+    for object in databaseEntries:
+        if object["properties"][config["DATE_PROPERTY"]]["date"] is not None:
+            filtered.append(
+                [
+                    {
+                        "title": object["properties"][config["TITLE_PROPERTY"]][
+                            "title"
+                        ][0]["text"]["content"],
+                        "date": object["properties"][config["DATE_PROPERTY"]]["date"],
+                    }
+                ]
+            )
+        else:
+            print(
+                f"Date property named {config['DATE_PROPERTY']} is empty for this Database, verify your config.json file."
+            )
+            exit()
+    return filtered
+
+
+if __name__ == "__main__":
+    notion = Client(auth=config["NOTION_TOKEN"])
+
+    database_entries = getDatabaseEntries(notion)
+    decoded_data = decode(database_entries)
+
+    print(decoded_data)
